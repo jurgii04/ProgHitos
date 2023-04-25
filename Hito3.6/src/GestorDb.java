8d76b348fe9bbda7e5a6b780526e75e7fe83d4e8
ik012982i9 <oslimani22dw@ikzubiri.manteo.com>
2023-04-25 09:23:38 +0200

    Hito 3.6 terminado

--- Hito3.6/src/GestorDb.java
+++ Hito3.6/src/GestorDb.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class GestorDB {
+import java.sql.*;
+import java.util.ArrayList;
+
+public class GestorDb {
+    static final String SERVER_IP = "localhost";
+    static final String DB_NAME = "hito3_6";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://" + SERVER_IP + ":3306/" + DB_NAME;
+    static final String USER = "root";
+    static final String PASS = "zubiri";
+    private Connection conn;
+    private Statement st;
+    private ResultSet resu;
+
+    public GestorDb() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Conectado");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String[] select(String tabla, String columna, String condicion) {
+        String consulta = "SELECT " + columna + " FROM " + tabla + " WHERE " + condicion;
+        if (condicion==null){
+             consulta = "SELECT " + columna + " FROM " + tabla;
+        }
+
+        ArrayList<String> valores = new ArrayList<>();
+        try {
+            st = conn.createStatement();
+            resu = st.executeQuery(consulta);
+            while (resu.next()) {
+                valores.add(resu.getString(columna));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] valoresStr = valores.toArray(new String[0]);
+
+        return valoresStr;
+    }
+
+    public static void main(String[] args) {
+        GestorDb gestorDb = new GestorDb();
+
+    }
 }
--- Hito3.6/src/PictureViewer.java
+++ Hito3.6/src/PictureViewer.java
@@ -1,109 +1,116 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import org.jdesktop.swingx.JXDatePicker;
-import java.text.SimpleDateFormat;
+
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 public class PictureViewer extends JFrame {
-    private JComboBox<Photographer> photographerComboBox;
+    private JComboBox<String> photographerComboBox;
     private JXDatePicker datePicker;
-    private JList<Picture> pictureList;
+    private JList<String> pictureList;
     private JLabel pictureLabel;
 
     private Connection conn;
 
-    public PictureViewer() {
-        super("Picture Viewer");
+    GestorDb gestorDb;
 
-        // Establecer el layout del marco
-        setLayout(new GridLayout(2, 2, 5, 5));
+    public PictureViewer() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Photography");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Layout
+        frame.setLayout(new GridLayout(2, 2, 5, 5));
 
         // Crear el combo box de fotógrafos
+        JPanel panel1 = new JPanel();
+        gestorDb = new GestorDb();
         photographerComboBox = new JComboBox<>();
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/my_database?user=root&password=root");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Photographers");
-            while (rs.next()) {
-                int photographerId = rs.getInt("PhotographerId");
-                String name = rs.getString("Name");
-                boolean awarded = rs.getBoolean("Awarded");
-                Photographer photographer = new Photographer(photographerId, name, awarded);
-                photographerComboBox.addItem(photographer);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+
+        String[] nombres = gestorDb.select("photographers", "nombre", null);
+
+        for (String nombre : nombres) {
+            photographerComboBox.addItem(nombre);
         }
-        add(photographerComboBox);
+
+        photographerComboBox.setPreferredSize(new Dimension(200, 25));
+        panel1.add(new JLabel("Photographer: "));
+        panel1.add(photographerComboBox);
+        frame.add(panel1);
 
         // Crear el componente de selección de fecha
+        JPanel panel2 = new JPanel();
         datePicker = new JXDatePicker();
-        add(datePicker);
+        panel2.add(new JLabel("Photos after: "));
+        panel2.add(datePicker);
+        frame.add(panel2);
 
         // Crear la lista de imágenes
+        JPanel panel3 = new JPanel();
         pictureList = new JList<>();
-        add(new JScrollPane(pictureList));
+        pictureList.setPreferredSize(new Dimension(300, 200));
+        panel3.add(new JScrollPane(pictureList));
+        frame.add(panel3);
 
         // Crear la etiqueta de imagen
+        JPanel panel4 = new JPanel();
         pictureLabel = new JLabel();
         pictureLabel.setHorizontalAlignment(JLabel.CENTER);
-        add(pictureLabel);
+        pictureLabel.setPreferredSize(new Dimension(300, 200));
+        panel4.add(pictureLabel);
+        frame.add(panel4);
 
-        // Configurar el comportamiento del combo box de fotógrafos
         photographerComboBox.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                Photographer selectedPhotographer = (Photographer) photographerComboBox.getSelectedItem();
-                try {
-                    Statement stmt = conn.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM Pictures WHERE PhotographerId = " + selectedPhotographer.getId());
-                    DefaultListModel<Picture> listModel = new DefaultListModel<>();
-                    while (rs.next()) {
-                        int pictureId = rs.getInt("PictureId");
-                        String title = rs.getString("Title");
-                        Date date = rs.getDate("Date");
-                        String file = rs.getString("File");
-                        int visits = rs.getInt("Visits");
-                        Picture picture = new Picture(pictureId, title, date, file, visits, selectedPhotographer);
-                        listModel.addElement(picture);
-                    }
-                    pictureList.setModel(listModel);
-                    pictureLabel.setIcon(null);
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+                // Obtener la fecha seleccionada del datePicker
+                Date fecha = datePicker.getDate();
+
+                // Crear un objeto SimpleDateFormat con el formato deseado
+                SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+
+                // Formatear la fecha como un String en el formato deseado
+                String fechaFormateada = formato.format(fecha);
+                String name = photographerComboBox.getSelectedItem().toString();
+                String [] titulos = gestorDb.select("pictures", "Title","PhotographerId IN (SELECT PhotographerId FROM photographers WHERE nombre = '" + name + "' AND fecha > '" + fechaFormateada + "')");
+
+
+                pictureList.setListData(titulos);
             }
         });
+
 
         // Configurar el comportamiento de la lista de imágenes
         pictureList.addListSelectionListener(new ListSelectionListener() {
+            @Override
             public void valueChanged(ListSelectionEvent e) {
-                Picture selectedPicture = (Picture) pictureList.getSelectedValue();
-                if (selectedPicture != null) {
-                    ImageIcon imageIcon = new ImageIcon(selectedPicture.getFile());
-                    Image image = imageIcon.getImage();
-                    Image scaledImage = image.getScaledInstance(pictureLabel.getWidth(), pictureLabel.getHeight(), Image.SCALE_SMOOTH);
-                    ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
-                    pictureLabel.setIcon(scaledImageIcon);
-                } else {
-                    pictureLabel.setIcon(null);
-                }
+                String titulo = pictureList.getSelectedValue();
+                String src = Arrays.toString(gestorDb.select("pictures", "File","title = '" + titulo + "'"));
+                String file = src.substring(1,src.length()-1);
+
+                ImageIcon icono = new ImageIcon(file);
+                int labelWidth = pictureLabel.getWidth();
+                int labelHeight = pictureLabel.getHeight();
+                Image img = icono.getImage();
+                Image scaledImg = img.getScaledInstance(labelWidth, labelHeight, Image.SCALE_SMOOTH);
+                icono = new ImageIcon(scaledImg);
+                pictureLabel.setIcon(icono);
             }
         });
 
-        // Configurar el marco
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new PictureViewer();
-            }
-        });
+        PictureViewer pictureViewer = new PictureViewer();
     }
 }
 
